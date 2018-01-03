90c5b7f925260911150b1c2f8c1750623fa8181a
Kirill Shevchenko <kirills167@gmail.com>
2018-01-03 12:59:34 +0200

    add configurable urls

--- lib/api_struct/client.rb
+++ lib/api_struct/client.rb
@@ -4,12 +4,6 @@ module ApiStruct
 
     HTTP_METHODS = %i[get post patch put delete].freeze
 
-    def self.url(value)
-      define_method :url do
-        value
-      end
-    end
-
     def self.headers(value)
       define_method :headers do
         value
@@ -25,12 +19,17 @@ module ApiStruct
       @client = HTTP::Client.new(headers: headers)
     end
 
+    def self.method_missing(m, *args, &block)
+      endpoints = Settings.config.endpoints
+      endpoints.keys.include?(m) ? define_method(:url) { endpoints[m] + first_arg(args) } : super
+    end
+
     HTTP_METHODS.each do |http_method|
       define_method http_method do |*args|
         begin
-          wrap client.send(http_method, *args)
-        rescue HTTP::ConnectionError => error
-          failure(body: error.message, status: :not_connected)
+          wrap client.send(http_method, (url + first_arg(args)) )
+        rescue HTTP::ConnectionError => e
+          failure(body: e.message, status: :not_connected)
         end
       end
     end
@@ -50,6 +49,10 @@ module ApiStruct
     def failure(response)
       result = ApiStruct::Errors::Client.new(response)
       Dry::Monads.Left(result)
+    end
+
+    def first_arg(args)
+      args.first.to_s
     end
   end
 end
